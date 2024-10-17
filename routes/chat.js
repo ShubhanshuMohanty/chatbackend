b07eb5b919669f88c39c7fb250b45742db6cd1c9
shubhanshu <shubhanshumohanty@gmail.com>
2024-10-17 12:57:24 +0530

    add remove member route

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { addMembers, getMyChats, getMyGroups, newGroupChat } from "../controllers/chat.js";
+import { addMembers, getMyChats, getMyGroups, newGroupChat, removeMember } from "../controllers/chat.js";
 
 const app = express.Router();
 
@@ -11,5 +11,6 @@ app.get("/my",getMyChats)
 app.get("/my/group",getMyGroups)
 
 app.put("/addmembers",addMembers)
+app.put("/removemember",removeMember)
 
 export default app;
