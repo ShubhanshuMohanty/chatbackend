4431edd865954fd24fa590ef4afd47820e83afeb
shubhanshu <shubhanshumohanty@gmail.com>
2024-10-19 15:12:47 +0530

    write all validator

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,11 @@ import express from "express";
 import { getMyProfile, login,logout,newUser, searchUser } from "../controllers/user.js";
 import { multerUpload, singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { registerValidator, validateHandler } from "../lib/validators.js";
 
 const app=express.Router();
 
-app.post("/new",singleAvatar,newUser);
+app.post("/new",singleAvatar,registerValidator(),validateHandler,newUser);
 app.post('/login',login );
 
 app.use(isAuthenticated)
