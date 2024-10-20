c0c0e674cd890d42e96b9fd0bb0747975a47a501
shubhanshu <shubhanshumohanty@gmail.com>
2024-10-20 15:48:57 +0530

    add all admin route and controller

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ import cookieParser from "cookie-parser";
 import UserRoute from "./routes/user.js";
 import ChatRoute from "./routes/chat.js";
 import AdminRoute from "./routes/admin.js";
-import { createGroupChats, createMessagesInAChat, createSingleChats } from "./seeders/chat.js";
+import {
+  createGroupChats,
+  createMessagesInAChat,
+  createSingleChats,
+} from "./seeders/chat.js";
 // import { createUser } from "./seeders/user.js";
 
 dotenv.config({
-    path: './.env',
-})
-const mongoURI= process.env.MONGO_URI;
+  path: "./.env",
+});
+const mongoURI = process.env.MONGO_URI;
+const adminSecretKey = process.env.ADMIN_SECRET_KEY || "adsasdsdfsdfsdfd";
 connectDB(mongoURI);
 // createUser(10);
 // createSingleChats(20);
@@ -26,21 +31,21 @@ const app = express();
 //using middleware
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 // app.use(express.urlencoded({ extended: true }));
 
+app.use("/user", UserRoute);
+app.use("/chat", ChatRoute);
+app.use("/admin", AdminRoute);
 
+app.use(errorMiddleware);
 
-app.use("/user",UserRoute)
-app.use("/chat",ChatRoute);
-app.use("/admin",AdminRoute);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use(errorMiddleware)
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
-})
-
-app.listen(PORT,()=>{
-    console.log("Server is running on port 3000");
-})
+export { adminSecretKey };
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,38 @@
 import jwt from "jsonwebtoken";
 import { ErrorHandler } from "../utils/utility.js";
 import { TryCatch } from "./error.js";
+import { adminSecretKey } from "../app.js";
 
 
-const isAuthenticated =TryCatch(async(req,res,next)=>{
+const isAuthenticated = TryCatch(async (req, res, next) => {
+  // console.log("cookies:"+req.cookies["chattu-token"]);
 
-    // console.log("cookies:"+req.cookies["chattu-token"]);
-    
-    
-    
-    const token=req.cookies["chattu-token"];
-    if(!token) return next(new ErrorHandler("please login to acces this route",401))
+  const token = req.cookies["chattu-token"];
+  if (!token)
+    return next(new ErrorHandler("please login to acces this route", 401));
 
-    const decodedData=jwt.verify(token,process.env.JWT_SECRET);
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log("decoded data:",decodedData);
-    req.user=decodedData._id;
-    
-    next();
-})
+  console.log("decoded data:", decodedData);
+  req.user = decodedData._id;
 
-export{
-    isAuthenticated,
-}
+  next();
+});
+
+const adminOnly = (req, res, next) => {
+  const token = req.cookies["chattu-admin-token"];
+
+  if (!token)
+    return next(new ErrorHandler("Only Admin can access this route", 401));
+
+  const secretKey = jwt.verify(token, process.env.JWT_SECRET);
+
+  const isMatched = secretKey === adminSecretKey;
+
+  if (!isMatched)
+    return next(new ErrorHandler("Only Admin can access this route", 401));
+
+  next();
+};
+
+export { isAuthenticated ,adminOnly};
